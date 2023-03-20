7cf70682adf907cf73b6a28703c2fa12a2ea5eeb
Juan Vera <juanfrvera@gmail.com>
2023-03-20 15:45:09 -0300

    feat: isManual payDate difference

--- src/routes/item-see/renders/service-see-render.svelte
+++ src/routes/item-see/renders/service-see-render.svelte
@@ -4,58 +4,64 @@
 
 	export let data: IItemData<IServiceItemConfig>;
 
-	let view: {
-		lastPayDateString: string;
-		showMarkAsPaidButton: boolean;
-		markPaidDatePicker?: {
-			dateInputString: string;
-		};
-		showChangePaidDateButton: boolean;
-	};
-
+	let view: IView;
 	onMount(() => {
-		let lastPayDateString = 'Never';
-		let showMarkAsPaidButton = true;
-		if (data.config.lastPayDate != null) {
-			const lastPayDate = data.config.lastPayDate;
-			lastPayDateString = lastPayDate.toLocaleDateString();
+		view = {};
+		if (data.config.isManual) {
+			let lastPayDateString = 'Never';
+			let showMarkAsPaidButton = true;
+			if (data.config.lastPayDate != null) {
+				const lastPayDate = data.config.lastPayDate;
+				lastPayDateString = lastPayDate.toLocaleDateString();
 
-			const date = new Date();
-			if (
-				lastPayDate.getMonth() === date.getMonth() &&
-				lastPayDate.getFullYear() === date.getFullYear()
-			) {
-				showMarkAsPaidButton = false;
+				const date = new Date();
+				if (
+					lastPayDate.getMonth() === date.getMonth() &&
+					lastPayDate.getFullYear() === date.getFullYear()
+				) {
+					showMarkAsPaidButton = false;
+				}
 			}
-		}
 
-		view = {
-			lastPayDateString,
-			showMarkAsPaidButton,
-			showChangePaidDateButton: !showMarkAsPaidButton
-		};
+			view.payDate = {
+				lastPayDateString,
+				showMarkAsPaidButton,
+				showChangePaidDateButton: !showMarkAsPaidButton
+			};
+		}
 	});
 
 	function markAsPaidClicked() {
-		view.showMarkAsPaidButton = false;
+		view.payDate!.showMarkAsPaidButton = false;
 		openMarkPaidDatePicker(new Date(Date.now()));
 	}
 	function changePaidDateClicked() {
-		view.showChangePaidDateButton = false;
+		view.payDate!.showChangePaidDateButton = false;
 		openMarkPaidDatePicker(data.config.lastPayDate);
 	}
 	function confirmPaidDate() {
-		const date = new Date(view.markPaidDatePicker!.dateInputString + 'T00:00');
-		view.lastPayDateString = date.toLocaleDateString();
+		const date = new Date(view.payDate!.markPaidDatePicker!.dateInputString + 'T00:00');
+		view.payDate!.lastPayDateString = date.toLocaleDateString();
 		data.config.lastPayDate = date;
 
-		view.markPaidDatePicker = undefined;
-		view.showChangePaidDateButton = true;
+		view.payDate!.markPaidDatePicker = undefined;
+		view.payDate!.showChangePaidDateButton = true;
 	}
 	function openMarkPaidDatePicker(date: Date) {
 		// We use a 10 length string to cut the time from the date so it ends up being yyyy/mm/dd
-		view.markPaidDatePicker = {
+		view.payDate!.markPaidDatePicker = {
 			dateInputString: date.toISOString().substring(0, 10)
+		};
+	}
+
+	interface IView {
+		payDate?: {
+			lastPayDateString: string;
+			showMarkAsPaidButton: boolean;
+			markPaidDatePicker?: {
+				dateInputString: string;
+			};
+			showChangePaidDateButton: boolean;
 		};
 	}
 </script>
@@ -64,19 +70,21 @@
 	<div class="service-see">
 		<div>{data.config.name}</div>
 		<div>{data.config.cost} {data.config.currency}</div>
-		<div class="label-and-value">
-			<div class="value-label">Last pay date:</div>
-			<div class="value">{view.lastPayDateString}</div>
-		</div>
-		{#if view.showMarkAsPaidButton}
-			<button on:click={markAsPaidClicked}>Mark as Paid</button>
-		{/if}
-		{#if view.markPaidDatePicker != undefined}
-			<input bind:value={view.markPaidDatePicker.dateInputString} type="date" />
-			<button on:click={confirmPaidDate}>Confirm date</button>
-		{/if}
-		{#if view.showChangePaidDateButton}
-			<button on:click={changePaidDateClicked}>Change Date</button>
+		{#if view.payDate != undefined}
+			<div class="label-and-value">
+				<div class="value-label">Last pay date:</div>
+				<div class="value">{view.payDate.lastPayDateString}</div>
+			</div>
+			{#if view.payDate.showMarkAsPaidButton}
+				<button on:click={markAsPaidClicked}>Mark as Paid</button>
+			{/if}
+			{#if view.payDate.markPaidDatePicker != undefined}
+				<input bind:value={view.payDate.markPaidDatePicker.dateInputString} type="date" />
+				<button on:click={confirmPaidDate}>Confirm date</button>
+			{/if}
+			{#if view.payDate.showChangePaidDateButton}
+				<button on:click={changePaidDateClicked}>Change Date</button>
+			{/if}
 		{/if}
 	</div>
 {/if}
