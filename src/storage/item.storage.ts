6e611c7f4c497f104387582c88b9d59f9be9bbfb
Juan Vera <juanfrvera@gmail.com>
2023-03-27 22:05:30 -0300

    wip: search bar and separated item page

---
+++ src/storage/item.storage.ts
@@ -0,0 +1,16 @@
+import type { IItemData } from "../typings";
+
+export class ItemStorage {
+    private static readonly itemsLocalStorageKey = "items";
+
+    public static getItems(): IItemData[] {
+        const listJsonString = localStorage.getItem(this.itemsLocalStorageKey);
+        if (listJsonString != null) {
+            return JSON.parse(listJsonString);
+        }
+        return [];
+    }
+    public static saveItems(items: IItemData[]) {
+        localStorage.setItem(this.itemsLocalStorageKey, JSON.stringify(items));
+    }
+}
