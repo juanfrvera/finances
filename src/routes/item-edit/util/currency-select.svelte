760ce2004061c9bd73c788df44487eae1c045397
Juan Vera <juanfrvera@gmail.com>
2023-04-26 09:14:37 -0300

    feat: save created currency

--- src/routes/item-edit/util/currency-select.svelte
+++ src/routes/item-edit/util/currency-select.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { getContext, onMount } from 'svelte';
 	import { CurrencyLogic, type ICurrencyContext } from '../../../logic/currency.logic';
+	import { listen } from 'svelte/internal';
 
 	export let value: string = 'ars';
 
@@ -14,6 +15,8 @@
 			disableSave?: boolean;
 		};
 	} = {};
+
+	let createdCurrency: string;
 
 	onMount(() => {
 		view.list = context.getCurrencies();
@@ -30,16 +33,48 @@
 			disableSave: true
 		};
 	}
+
+	function creationInputKeyUp(e: KeyboardEvent) {
+		const value = (e.target as HTMLInputElement).value;
+
+		view.creating!.disableSave = value == null || value.length <= 0;
+
+		createdCurrency = value;
+	}
+
+	function cancelCreation() {
+		closeCreation();
+	}
+
+	function saveCreation() {
+		context.addCurrency(createdCurrency);
+
+		if (view.list != null) {
+			view.list.push(createdCurrency);
+		} else {
+			view.list = [createdCurrency];
+		}
+
+		closeCreation();
+	}
+
+	function closeCreation() {
+		createdCurrency = '';
+		view.creating = undefined;
+		view.showList = true;
+		view.showCreateButton = true;
+	}
 </script>
 
 <div class="label-and-input">
 	<label for="currency-input">Currency</label>
 	<div class="select-and-button">
-		{#if view.showList}
+		{#if view.showList && view.list}
 			<div class="select input-stretch">
 				<select bind:value id="currency-input" class="w-100">
-					<option>ars</option>
-					<option>usd</option>
+					{#each view.list as currency}
+						<option>{currency}</option>
+					{/each}
 				</select>
 			</div>
 		{/if}
@@ -47,9 +82,9 @@
 			<button on:click={createButtonClicked}>Create Currency</button>
 		{/if}
 		{#if view.creating}
-			<input class="input" type="text" placeholder="usd" />
-			<button disabled={view.creating.disableSave}>Save</button>
-			<button>Cancel</button>
+			<input on:keyup={creationInputKeyUp} class="input" type="text" placeholder="usd" />
+			<button disabled={view.creating.disableSave} on:click={saveCreation}>Save</button>
+			<button on:click={cancelCreation}>Cancel</button>
 		{/if}
 	</div>
 </div>
