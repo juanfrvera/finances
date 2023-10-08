7782077dba6f89ad20fa0778d27f9371199d1e6f
Juan <juanfrvera@gmail.com>
2023-10-08 11:05:29 -0300

    feat: better buttons on service pay

--- src/lib/components/item/item-see/renders/service-see-render.svelte
+++ src/lib/components/item/item-see/renders/service-see-render.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { ServiceItem } from '@/lib/util/logic/item';
-	import type { iItem, IServiceConfig } from '../../../../typings';
+	import type { IService } from '@/lib/typings';
 
 	export let data: IService;
 	const dispatch = createEventDispatcher();
 
-	let view: IView;
+	let ui: IView;
 	onMount(() => {
-		view = {};
+		ui = {};
 		if (data.isManual) {
 			let lastPayDateString = 'Never';
 			let showMarkAsPaidButton = true;
@@ -18,7 +18,7 @@
 				lastPayDateString = data.lastPayDateString;
 			}
 
-			view.payDate = {
+			ui.payDate = {
 				lastPayDateString,
 				showMarkAsPaidButton,
 				showChangePaidDateButton: !showMarkAsPaidButton
@@ -27,30 +27,35 @@
 	});
 
 	function markAsPaidClicked() {
-		view.payDate!.showMarkAsPaidButton = false;
+		ui.payDate!.showMarkAsPaidButton = false;
 		openMarkPaidDatePicker(new Date(Date.now()));
 	}
 	function changePaidDateClicked() {
-		view.payDate!.showChangePaidDateButton = false;
+		ui.payDate!.showChangePaidDateButton = false;
 		openMarkPaidDatePicker(new Date(data.lastPayDateString));
 	}
 	function confirmPaidDate() {
-		const date = new Date(view.payDate!.markPaidDatePicker!.dateInputString + 'T00:00');
+		const date = new Date(ui.payDate!.markPaidDatePicker!.dateInputString + 'T00:00');
 		data.lastPayDateString = date.toLocaleDateString();
-		view.payDate!.lastPayDateString = data.lastPayDateString;
+		ui.payDate!.lastPayDateString = data.lastPayDateString;
 
-		view.payDate!.markPaidDatePicker = undefined;
-		view.payDate!.showChangePaidDateButton = true;
+		ui.payDate!.markPaidDatePicker = undefined;
+		ui.payDate!.showChangePaidDateButton = true;
 		triggerOnUpdate();
 	}
 	function openMarkPaidDatePicker(date: Date) {
 		// We use a 10 length string to cut the time from the date so it ends up being yyyy/mm/dd
-		view.payDate!.markPaidDatePicker = {
+		ui.payDate!.markPaidDatePicker = {
 			dateInputString: date.toISOString().substring(0, 10)
 		};
 	}
 	function triggerOnUpdate() {
 		dispatch('update', data);
+	}
+
+	function cancelPaidDateMark() {
+		ui.payDate!.markPaidDatePicker = undefined;
+		ui.payDate!.showMarkAsPaidButton = true;
 	}
 
 	interface IView {
@@ -65,23 +70,32 @@
 	}
 </script>
 
-{#if view != null}
+{#if ui != null}
 	<div class="service-see">
 		<div>{data.name}</div>
 		<div>{data.cost} {data.currency}</div>
-		{#if view.payDate != undefined}
+		{#if ui.payDate != undefined}
 			<div class="label-and-value">
 				<div class="value-label">Last pay date:</div>
-				<div class="value">{view.payDate.lastPayDateString}</div>
+				<div class="value">{ui.payDate.lastPayDateString}</div>
 			</div>
-			{#if view.payDate.showMarkAsPaidButton}
+			{#if ui.payDate.showMarkAsPaidButton}
 				<button on:click={markAsPaidClicked} class="button">Mark as Paid</button>
 			{/if}
-			{#if view.payDate.markPaidDatePicker != undefined}
-				<input bind:value={view.payDate.markPaidDatePicker.dateInputString} type="date" />
-				<button on:click={confirmPaidDate} class="button">Confirm date</button>
+			{#if ui.payDate.markPaidDatePicker != undefined}
+				<div class="mark-paid-window">
+					<input
+						bind:value={ui.payDate.markPaidDatePicker.dateInputString}
+						type="date"
+						class="input"
+					/>
+					<div class="mark-paid-window__footer">
+						<button on:click={confirmPaidDate} class="button">Confirm date</button>
+						<button on:click={cancelPaidDateMark} class="button">Cancel</button>
+					</div>
+				</div>
 			{/if}
-			{#if view.payDate.showChangePaidDateButton}
+			{#if ui.payDate.showChangePaidDateButton}
 				<button on:click={changePaidDateClicked} class="button">Change Date</button>
 			{/if}
 		{/if}
@@ -99,5 +113,11 @@
 	.label-and-value {
 		display: flex;
 		column-gap: 8px;
+	}
+
+	.mark-paid-window__footer {
+		margin-top: 16px;
+		display: flex;
+		gap: 16px;
 	}
 </style>
